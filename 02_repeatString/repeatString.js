d312c44ab4f6cd6f1eaddcb2b650306ceb049627
Mbk <aakashpr1c2alo@gmail.com>
2021-11-09 09:15:44 +0530

    02 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let text = string;
+	if (num < 0) {
+		return "ERROR";
+	} else if (num == 0) {
+		return "";
+	} else {
+		for (let i = 1; i < num; i++) {
+			text += string;
+		}
+		return text;
+	}
 };
 
 // Do not edit below this line
