a36ec570d5dcfb5a5a051d5f0e7cdfc02c8f1313
Mbk <aakashpr1c2alo@gmail.com>
2021-11-09 19:34:26 +0530

    04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...rem) {
+	let num = array.length;
+	for (let j = 0; j < rem.length; j++) {
+		for (let i = 0; i < num; i++) {
+			if (array[i] === rem[j]) {
+				array.splice(i, 1);
+			}
+		}
+	}
+	return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
