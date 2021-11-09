79d06b41da16219188e11f32d8d321edcb6e51d7
Mbk <aakashpr1c2alo@gmail.com>
2021-11-09 09:27:32 +0530

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+    let reverseStr;
+    
+    for (let i = str.length-1, let j = 0; i>=0; i--, j++) {
+        reverseStr[j] = str[i];
 
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
