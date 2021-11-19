53ad080a17b1ed5d1fae013c22edc3c36100b881
Mbk <aakashpr1c2alo@gmail.com>
2021-11-19 15:03:21 +0530

    fibo done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (element) {
+	element = parseInt(element);
+	if (element >= 0) {
+		if (element === 0) return 0;
+		if (element === 1) return 1;
 
+		return fibonacci(element - 2) + fibonacci(element - 1);
+	} else {
+		return "OOPS";
+	}
 };
 
 // Do not edit below this line
