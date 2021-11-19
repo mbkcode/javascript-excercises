05147766842711c644ee703967e07f035fede508
Mbk <aakashpr1c2alo@gmail.com>
2021-11-19 16:23:55 +0530

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+	return array.reduce((a, b) => {
+		const lastGuy = getAge(a.yearOfBirth, a.yearOfDeath);
+		const nextGuy = getAge(b.yearOfBirth, b.yearOfDeath);
+		return lastGuy > nextGuy ? a : b;
+	});
+};
 
+const getAge = function (birth, death) {
+	if (!death) {
+		death = new Date().getFullYear();
+	}
+	return death - birth;
 };
 
 // Do not edit below this line
