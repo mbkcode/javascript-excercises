66854d0b8c3a78183c0e7cc8422210ecdf575ce4
Mbk <aakashpr1c2alo@gmail.com>
2021-11-19 14:07:19 +0530

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+	let sum = a + b;
+	return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	let difference = a - b;
+	return difference;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	return array.reduce(
+		(previousValue, currentValue) => previousValue * currentValue,
+		1
+	);
 };
 
-const power = function() {
-	
+const power = function (a, x) {
+	return Math.pow(a, x);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+	if (a == 0 || a == 1) {
+		return 1;
+	} else {
+		let factorial = 1;
+		for (; a > 0; a--) {
+			factorial *= a;
+		}
+		return factorial;
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
